940ab20563b8986c1457b0b10b45dc4dc941650c
Daniel Fabián <daniel.elbelaizi.alu@iescampanillas.com>
2021-02-13 19:44:06 +0100

    Nueva Linea

--- JavaGit/src/javaGit.java
+++ JavaGit/src/javaGit.java
@@ -1,6 +1,6 @@
 public class javaGit {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
-		System.out.println("Segunda Linea"); 
+		System.out.println("Segunda Linea");
 	}
 }
