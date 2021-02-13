5b801d224ccd1c03da6d5da9bcb9e2ec44708ae4
Daniel Fabián <72435753+Danny-06@users.noreply.github.com>
2021-02-13 19:51:27 +0100

    Update javaGit.java

--- JavaGit/src/javaGit.java
+++ JavaGit/src/javaGit.java
@@ -3,5 +3,7 @@ public class javaGit {
 		System.out.println("Hola Mundo");
 		System.out.println("Segunda Linea");
 		System.out.println("Tercera Linea");
+		
+		System.out.println((int)Math.random() * 11);
 	}
 }
